413cb51a8323c4832db75ddae65a0c51aa13a9cd
Chun-gu <chungu.yi92@gmail.com>
2021-12-19 23:17:20 +0900

    add pages

---
+++ src/pages/wines/sparkling.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const SparklingPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Sparkling Wines</h1>
+        </div>
+    );
+};
+
+export default SparklingPage;
---
+++ src/pages/wines/whites.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const WhitesPage: NextPage = () => {
+    return (
+        <div>
+            <h1>White Wines</h1>
+        </div>
+    );
+};
+
+export default WhitesPage;
