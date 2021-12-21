ea840801852b5ea3cb50de7eedeef5eeef9c6c75
Chun-gu <chungu.yi92@gmail.com>
2021-12-21 20:06:55 +0900

    edit file name

---
+++ src/pages/wines/desserts.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const DessertsPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Desserts</h1>
+        </div>
+    );
+};
+
+export default DessertsPage;
